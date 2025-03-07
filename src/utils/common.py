1b1c64996e6bbd5ed83cf28906a4bbea39f523e3
JBC2024 <jbarcoc@gmail.com>
2025-03-07 12:03:21 +0100

    Librería de utilidades

---
+++ src/utils/common.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+TRAIN_PATH = "../data/mpg_train.csv"
+
+def get_dataframe():
+    df_result = pd.read_csv(TRAIN_PATH)
+
+    new_names = ["year", "brand", "model", "vehicle_class", "engine_size", "cylinders", "transmission", "fuel_type", "fuel_city_Lkm", "fuel_hwy_Lkm", "fuel_comb_Lkm", "fuel_comb_mpg", "co2"]
+    df_result.columns = new_names
+
+    return df_result
+
+def data_report(df):
+    '''Esta funcion describe los campos de un dataframe de pandas de forma bastante clara, crack'''
+    # Sacamos los NOMBRES
+    cols = pd.DataFrame(df.columns.values, columns=["COL_N"])
+
+    # Sacamos los TIPOS
+    types = pd.DataFrame(df.dtypes.values, columns=["DATA_TYPE"])
+
+    # Sacamos los MISSINGS
+    percent_missing = round(df.isnull().sum() * 100 / len(df), 2)
+    percent_missing_df = pd.DataFrame(percent_missing.values, columns=["MISSINGS (%)"])
+
+    # Sacamos los VALORES UNICOS
+    unicos = pd.DataFrame(df.nunique().values, columns=["UNIQUE_VALUES"])
+    
+    percent_cardin = round(unicos['UNIQUE_VALUES']*100/len(df), 2)
+    percent_cardin_df = pd.DataFrame(percent_cardin.values, columns=["CARDIN (%)"])
+
+    concatenado = pd.concat([cols, types, percent_missing_df, unicos, percent_cardin_df], axis=1, sort=False)
+    concatenado.set_index('COL_N', drop=True, inplace=True)
+
+    return concatenado.T
